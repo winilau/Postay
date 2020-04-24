524508e894282d98da789ed760d72ca13e566911
winilau <winilau22@gmail.com>
2020-04-25 03:30:20 +0800

    main page

--- app/src/main/java/com/example/postay/main.java
+++ app/src/main/java/com/example/postay/main.java
@@ -3,12 +3,20 @@ package com.example.postay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class main extends AppCompatActivity {
+    private Button mVolunteer = findViewById(R.id.volunteer);
+    private Button mUser = findViewById(R.id.user);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
