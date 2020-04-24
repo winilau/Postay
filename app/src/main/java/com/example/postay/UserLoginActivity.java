be151a5fea56465a94625216117711e97084ab37
winilau <winilau22@gmail.com>
2020-04-25 05:02:40 +0800

    auth

--- app/src/main/java/com/example/postay/UserLoginActivity.java
+++ app/src/main/java/com/example/postay/UserLoginActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class VolunteerLogInActivity extends AppCompatActivity {
+public class UserLoginActivity extends AppCompatActivity {
     private EditText mEmail, mPassword;
     private Button mLogin,mRegister;
 
@@ -27,7 +27,7 @@ public class VolunteerLogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_volunteer_log_in);
+        setContentView(R.layout.activity_user_login);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -36,7 +36,7 @@ public class VolunteerLogInActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user != null){
-                    Intent intent = new Intent(VolunteerLogInActivity.this, MapsActivityVolunteer.class);
+                    Intent intent = new Intent(UserLoginActivity.this, MapsActivityUser.class);
                     startActivity(intent);
                     finish();
                     return;
@@ -55,14 +55,14 @@ public class VolunteerLogInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
-                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(VolunteerLogInActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(UserLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (!task.isSuccessful()){
-                            Toast.makeText(VolunteerLogInActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(UserLoginActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
                         }else{
                             String user_id = mAuth.getCurrentUser().getUid();
-                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("users").child("Volunteer").child(user_id);
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("users").child("UserUserLoginActivity").child(user_id);
                             current_user_db.setValue(true);
                         }
                     }
@@ -76,11 +76,11 @@ public class VolunteerLogInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
-                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(VolunteerLogInActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(UserLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (!task.isSuccessful()){
-                            Toast.makeText(VolunteerLogInActivity.this, "sign in error", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(UserLoginActivity.this, "sign in error", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
