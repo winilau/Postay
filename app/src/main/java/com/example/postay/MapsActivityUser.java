c9063fc9a4fcb9a4bef7920914e57df9d7752f99
kenza-amira <57840494+kenza-amira@users.noreply.github.com>
2020-04-25 03:18:23 +0000

    Added the finding a volunteer functionality

--- app/src/main/java/com/example/postay/MapsActivityUser.java
+++ app/src/main/java/com/example/postay/MapsActivityUser.java
@@ -14,6 +14,8 @@ import android.widget.Button;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
@@ -25,6 +27,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -73,11 +76,56 @@ public class MapsActivityUser extends FragmentActivity implements OnMapReadyCall
                 dropOff = new LatLng(mLocation.getLatitude(),mLocation.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(dropOff).title("Drop Off Here"));
 
-                mRequestButton.setText("Finding a Volunteer!");
+                mRequestButton.setText("Finding a Volunteer...");
+
+                getClosestVolunteer();
             }
         });
     }
 
+    private int radius = 1;
+    private Boolean volunteerFound = false;
+    private String volunteerFoundID;
+    private void getClosestVolunteer(){
+        DatabaseReference volunteerLoc = FirebaseDatabase.getInstance().getReference().child("volunteersAvailable");
+
+        GeoFire geo = new GeoFire(volunteerLoc);
+        GeoQuery query = geo.queryAtLocation(new GeoLocation(dropOff.latitude, dropOff.longitude),radius);
+        query.removeAllListeners();
+        query.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                if(!volunteerFound) {
+                    volunteerFound = true;
+                    volunteerFoundID = key;
+                    mRequestButton.setText("Volunteer Found!");
+                }
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                if (!volunteerFound){
+                    radius += 1;
+                    getClosestVolunteer();
+                }
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+
+            }
+        });
+    }
 
     /**
      * Manipulates the map once available.
