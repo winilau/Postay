49affce23709467484e7c3726d5c70a253343952
winilau <winilau22@gmail.com>
2020-04-26 02:54:40 +0800

    e

--- app/src/main/java/com/example/postay/UserShopping.java
+++ app/src/main/java/com/example/postay/UserShopping.java
@@ -9,22 +9,31 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewDebug;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class UserShopping extends AppCompatActivity {
 
     private ListView listView;
+    private Spinner spinner;
     private String mProduct[] = {"Milk","Bread","Toilet Paper","Pasta","Eggs","Laundry Pods","Ricotta Cheese"};
-    private double mPrice[] = {3.0, 1.6, 2.0, 1.5, 1.0, 1.5, 1.0};
+    private String mPrice[] = {"3.0", "1.6", "2.0", "1.5", "1.0", "1.5", "1.0"};
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_shopping);
 
+        spinner = findViewById(R.id.spinner);
+        String[] num = new String[]{"1","2","3","4","5","6","7","8","9","10"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,num);
+        spinner.setAdapter(adapter1);
         listView = findViewById(R.id.listView);
 
         MyAdapter adapter = new MyAdapter(this,mProduct,mPrice);
@@ -32,13 +41,14 @@ public class UserShopping extends AppCompatActivity {
 
     }
 
+
     class MyAdapter extends ArrayAdapter<String> {
 
         Context context;
         String rProduct[];
-        double rPrice[];
+        String rPrice[];
 
-        MyAdapter (Context c, String product[], double price[]){
+        MyAdapter (Context c, String product[], String price[]){
             super(c, R.layout.row, R.id.product, product);
             this.context = c;
             this.rProduct = product;
@@ -54,8 +64,7 @@ public class UserShopping extends AppCompatActivity {
             TextView myPrice = row.findViewById(R.id.price);
 
             myProduct.setText(rProduct[position]);
-            myPrice.setText(Double.toHexString(rPrice[position]));
-
+            myPrice.setText(rPrice[position]);
 
             return super.getView(position,convertView,parent);
         }
