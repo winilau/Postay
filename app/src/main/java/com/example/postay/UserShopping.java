5a9ef6a7a41e1746c5f9218734007e10e80265f4
winilau <winilau22@gmail.com>
2020-04-26 07:03:17 +0800

    e

--- app/src/main/java/com/example/postay/UserShopping.java
+++ app/src/main/java/com/example/postay/UserShopping.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewDebug;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,27 +23,34 @@ public class UserShopping extends AppCompatActivity {
 
     private ListView listView;
     private EditText amount;
-    private String mProduct[] = {"Milk","Bread","Toilet Paper","Pasta","Eggs","Laundry Pods","Ricotta Cheese"};
-    private String mPrice[] = {"3.0", "1.6", "2.0", "1.5", "1.0", "1.5", "1.0"};
-    private Button submit;
+    private String[] mProduct = {"Milk","Bread","Toilet Paper","Pasta","Eggs","Laundry Pods","Ricotta Cheese"};
+    private String[] mPrice = {"£3.0", "£1.6", "£2.0", "£1.5", "£1.0", "£1.5", "£1.0"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_shopping);
-        amount = (EditText) findViewById(R.id.amount);
+
+        listView = findViewById(R.id.listView);
         MyAdapter adapter = new MyAdapter(this,mProduct,mPrice);
         listView.setAdapter(adapter);
+
+        Button submit = new Button(this);
+        submit.setText("Submit");
+
+        listView.addFooterView(submit);
+
+
     }
 
 
     class MyAdapter extends ArrayAdapter<String> {
 
         Context context;
-        String rProduct[];
-        String rPrice[];
+        String[] rProduct;
+        String[] rPrice;
 
-        MyAdapter (Context c, String product[], String price[]){
+        MyAdapter (Context c, String[] product, String[] price){
             super(c, R.layout.row, R.id.product, product);
             this.context = c;
             this.rProduct = product;
@@ -56,10 +64,10 @@ public class UserShopping extends AppCompatActivity {
             View row = layoutInflater.inflate(R.layout.row,parent,false);
             TextView myProduct = row.findViewById(R.id.product);
             TextView myPrice = row.findViewById(R.id.price);
+
             myProduct.setText(rProduct[position]);
             myPrice.setText(rPrice[position]);
-
-            return super.getView(position,convertView,parent);
+            return row;
         }
 
     }
