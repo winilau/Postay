eb82cd3cc20d74d61a71b3659c86538a8b019b62
winilau <winilau22@gmail.com>
2020-04-26 12:42:55 +0800

    e

--- app/src/main/java/com/example/postay/UserShopping.java
+++ app/src/main/java/com/example/postay/UserShopping.java
@@ -31,8 +31,8 @@ import java.util.HashMap;
 
 public class UserShopping extends AppCompatActivity {
 
-    private ListView listView;
-    private EditText amount;
+    private EditText[] amount = new EditText[7];
+    private Button submit;
     private String[] mProduct = {"Milk","Bread","Toilet Paper","Pasta","Eggs","Laundry Pods","Ricotta Cheese"};
     private String[] mPrice = {"£3.0", "£1.6", "£2.0", "£1.5", "£1.0", "£1.5", "£1.0"};
 
@@ -41,18 +41,19 @@ public class UserShopping extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_shopping);
 
-        listView = findViewById(R.id.listView);
-        MyAdapter adapter = new MyAdapter(this,mProduct,mPrice);
-        listView.setAdapter(adapter);
-
-        Button submit = new Button(this);
-        submit.setText("Submit");
-
-        listView.addFooterView(submit);
-
         String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
         final DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("users")
                 .child("User").child(user_id);
+
+        submit = (Button) findViewById(R.id.submit);
+
+        amount[0] = findViewById(R.id.amount1);
+        amount[1] = findViewById(R.id.amount2);
+        amount[2] = findViewById(R.id.amount3);
+        amount[3] = findViewById(R.id.amount4);
+        amount[4] = findViewById(R.id.amount5);
+        amount[5] = findViewById(R.id.amount6);
+        amount[6] = findViewById(R.id.amount7);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,12 +63,11 @@ public class UserShopping extends AppCompatActivity {
                 HashMap<String,String> map  = new HashMap<>();
                 String countStr;
                 double count = 0.0;
-                for (int i = 0; i<mProduct.length; i++){
-                    View view = listView.getChildAt(i);
-                    String myAmount = view.findViewById(R.id.amount).toString();
+                for (int i = 0; i < 7; i++){
+                    String myAmount = amount[i].getText().toString();
                     if (!myAmount.matches("")){
-                        String item = view.findViewById(R.id.product).toString();
-                        String price = view.findViewById(R.id.price).toString();
+                        String item = mProduct[i];
+                        String price = mPrice[i];
                         map.put(item,myAmount);
                         double price1 = Double.parseDouble(price.substring(1));
                         int amount = Integer.parseInt(myAmount);
@@ -79,67 +79,5 @@ public class UserShopping extends AppCompatActivity {
                 total_price.child(countStr);
             }
         });
-    }
-
-    public class InputFilterMinMax implements InputFilter {
-
-        private int min, max;
-
-        public InputFilterMinMax(int min, int max) {
-            this.min = min;
-            this.max = max;
-        }
-
-        public InputFilterMinMax(String min, String max) {
-            this.min = Integer.parseInt(min);
-            this.max = Integer.parseInt(max);
-        }
-
-        @Override
-        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-            try {
-                int input = Integer.parseInt(dest.toString() + source.toString());
-                if (isInRange(min, max, input))
-                    return null;
-            } catch (NumberFormatException nfe) { }
-            return "";
-        }
-
-        private boolean isInRange(int a, int b, int c) {
-            return b > a ? c >= a && c <= b : c >= b && c <= a;
-        }
-    }
-
-
-    class MyAdapter extends ArrayAdapter<String> {
-
-        Context context;
-        String[] rProduct;
-        String[] rPrice;
-
-        MyAdapter (Context c, String[] product, String[] price){
-            super(c, R.layout.row, R.id.product, product);
-            this.context = c;
-            this.rProduct = product;
-            this.rPrice = price;
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = layoutInflater.inflate(R.layout.row,parent,false);
-            EditText myAmount = row.findViewById(R.id.amount);
-            TextView myProduct = row.findViewById(R.id.product);
-            TextView myPrice = row.findViewById(R.id.price);
-
-            myAmount.setFilters(new InputFilter[]{ new InputFilterMinMax("1", "12")});
-
-
-            myProduct.setText(rProduct[position]);
-            myPrice.setText(rPrice[position]);
-            return row;
-        }
-
     }
 }
